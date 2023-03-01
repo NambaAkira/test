1c6aa573523a7d646cf403dbdf99ff34ee128d13
ID-4306\HOUSEI4306 <namba@housei-inc.com>
2023-03-01 14:10:51 +0900

    upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/NambaAkira/test
+
+go 1.19
---
+++ test.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	str := "hello world"
+
+	fmt.Print(str, "\n")
+	fmt.Print("HELLO WORLD")
+}
