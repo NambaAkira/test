b9b5697eab1d5c618b30b98d715e63c70e1d821c
ID-4306\HOUSEI4306 <namba@housei-inc.com>
2023-03-07 12:58:52 +0900

    grpc実装

--- test.go
+++ test.go
@@ -2,9 +2,55 @@ package main
 
 import "fmt"
 
-func main() {
-	str := "hello world"
+type Int int
 
-	fmt.Print(str, "\n")
-	fmt.Print("おはようございます。\n")
+func (n *Int) Inc() {
+	*n++
+}
+
+type Person struct {
+	Name string
+}
+
+func (p Person) Greeting(msg string) {
+	fmt.Println("name(関数内)", &(p.Name))
+}
+
+func (p *Person) Greeting2(msg string) {
+	fmt.Println("name(関数内)", &(p.Name))
+}
+
+func (p Person) setName(name string) {
+	p.Name = name
+	fmt.Println("関数内1:", p.Name)
+}
+
+func (p *Person) setName2(name string) {
+	p.Name = name
+	fmt.Println("関数内2:", p.Name)
+
+}
+
+func main() {
+
+	var n Int
+	fmt.Println(n)
+	n.Inc()
+	fmt.Println(n)
+
+	/*
+		user := Person{"yuta"}
+		user.setName("yuuki")
+		fmt.Println(user.Name)
+
+		user2 := Person{"yuta"}
+		user2.setName2("yuuki")
+		fmt.Println(user2.Name)
+
+		println("-----------")
+
+		str := "hello world"
+		fmt.Print(str, "\n")
+		fmt.Print("おはようございます。\n")
+	*/
 }
